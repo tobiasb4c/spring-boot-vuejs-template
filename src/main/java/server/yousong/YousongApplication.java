7eca89bcf7cdb5d87e4f987cc7d882abec6d722d
franz stimpfl <franz.stimpfl@agilecoderz.com>
2022-09-20 11:59:28 +0200

    initial commit

---
+++ src/main/java/server/yousong/YousongApplication.java
@@ -0,0 +1,13 @@
+package server.yousong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YousongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YousongApplication.class, args);
+	}
+
+}
